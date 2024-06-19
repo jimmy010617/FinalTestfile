00143fff92ab9cb6f57eed998e7ae2919303bb39
jimmy010617 <jimmy990411@email.daelim.ac.kr>
2024-06-19 15:43:22 +0900

    시험2

--- src/Final.java
+++ src/Final.java
@@ -2,18 +2,23 @@
  * 이 프로그램은 swing을 이용하여 스크롤 윈도우 패널에 
  * 구구단을 출력하는 간단한 프로그램입니다.
  */
-
-// [문제1] swing 프로그램을 위해 필요한 컴포넌트 클래스 및 그래픽을 위한 클래스 import 해주세요.
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Final extends JFrame {
     private JTextArea resultArea;
 
     public Final() {
-        ; // [문제2] 타이틀에 '구구단 출력기'라고 나오도록 작성해 주세요.
-        ; // [문제3] 윈도우의 크기를 정의 하세요. 크기는 500 x 400입니다.
-        ; // [문제4] 윈도우 우측 상당의 X아이콘을 클릭하면 프로그램이 종료되도록 합니다.
+        setTitle("구구단");
+        setSize(500, 400);
+        WindowAdapter wa = new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+        this.addWindowListener(wa);
         setLocationRelativeTo(null);
 
         // 패널 생성
@@ -32,15 +37,21 @@ public class Final extends JFrame {
     }
 
     private void printGugudan() {
-        // [문제5] 구구단을 2단에서 9단까지 출력할 수 있도록 반복문을 작성하세요.
-        // 각 단마다 빈 줄을 추가해 주세요.
+        for(int i = 2; i < 10; i++) {
+            System.out.println(i + "단을 출력 합니다.");
+
+            for(int j = 1; j < 10; j++) {
+                System.out.println(i + " x " + j + " = "+ i * j);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                ; // [문제6] 결과 화면의 윈도우를 출력해 주세요.
+                new Final(); // [문제6] 결과 화면의 윈도우를 출력해 주세요.
             } 
         });
     }
